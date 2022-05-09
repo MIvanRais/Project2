18237afb6400d4f7e06b9f21d1a07a1643f6cd79
MIvanRais <26ivanrais@gmail.com>
2022-05-09 10:13:53 +0700

    add registration form validation and called inserData method in LogicBuilding class

--- AttendanceManagementSystem/src/presentation/administrationside/RegisterUser.java
+++ AttendanceManagementSystem/src/presentation/administrationside/RegisterUser.java
@@ -1,17 +1,15 @@
 package presentation.administrationside;
 
-import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
+
+import logic.LogicBuilding;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -20,14 +18,15 @@ import java.awt.event.ActionListener;
 public class RegisterUser extends JFrame implements ActionListener {
 
     JPanel panelNorth, panelCenter;
-    JLabel labelHeader, labelUsername, labelPassword, labelPassConfirm;
+    JLabel labelHeader, labelUsername, labelPassword, labelPassConfirm, labelWarnUsername, labelWarnPassword,
+            labelWarnConfirmPassword;
     JTextField textFieldUsername;
     JPasswordField passwordField, passwordFieldConfirm;
     JButton btnCreateAccount, btnBack;
 
     RegisterUser() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Create User");
+        this.setTitle("Create Account User");
         this.setSize(1200, 800);
         this.setResizable(false);
         this.setLayout(null);
@@ -38,7 +37,7 @@ public class RegisterUser extends JFrame implements ActionListener {
         panelNorth.setBackground(new Color(33, 72, 192));
 
         // **create labelHeader */
-        labelHeader = new JLabel("Create User");
+        labelHeader = new JLabel("Create Account User");
         labelHeader.setBounds(0, 0, 1200, 100);
         labelHeader.setFont(new Font(null, Font.BOLD, 25));
         labelHeader.setForeground(Color.white);
@@ -47,7 +46,7 @@ public class RegisterUser extends JFrame implements ActionListener {
         // **create panelCenter */
         panelCenter = new JPanel();
         panelCenter.setLayout(null);
-        panelCenter.setBounds(400, 150, 400, 360);
+        panelCenter.setBounds(400, 150, 400, 400);
         panelCenter.setBackground(Color.white);
 
         // **create labelUsername */
@@ -62,40 +61,41 @@ public class RegisterUser extends JFrame implements ActionListener {
         textFieldUsername.setSize(350, 35);
         textFieldUsername.setFont(new Font(null, Font.BOLD, 14));
 
+        // **create labelWarnUsername */
+        labelWarnUsername = new JLabel("Please Enter Your Username");
+        labelWarnUsername.setBounds(25, 80, 300, 50);
+        labelWarnUsername.setFont(new Font(null, Font.ITALIC, 12));
+        labelWarnUsername.setForeground(Color.red);
+
         // **create labelPassword */
         labelPassword = new JLabel("Password");
         labelPassword.setFont(new Font(null, Font.PLAIN, 14));
         labelPassword.setForeground(new Color(148, 164, 175));
-        labelPassword.setBounds(25, 100, 400, 45);
+        labelPassword.setBounds(25, 120, 400, 45);
 
         // **create passwordField */
         passwordField = new JPasswordField();
         passwordField.setSize(350, 35);
         passwordField.setFont(new Font(null, Font.BOLD, 14));
-        passwordField.setBounds(25, 135, 350, 35);
+        passwordField.setBounds(25, 155, 350, 35);
 
-        // **create labelPassConfirm */
-        labelPassConfirm = new JLabel("Confirm Password");
-        labelPassConfirm.setFont(new Font(null, Font.PLAIN, 14));
-        labelPassConfirm.setForeground(new Color(148, 164, 175));
-        labelPassConfirm.setBounds(25, 175, 400, 45);
-
-        // **create passwordFieldConfirm */
-        passwordFieldConfirm = new JPasswordField();
-        passwordFieldConfirm.setSize(350, 35);
-        passwordFieldConfirm.setFont(new Font(null, Font.BOLD, 14));
-        passwordFieldConfirm.setBounds(25, 210, 350, 35);
+        // **create labelWarnPassword */
+        labelWarnPassword = new JLabel("Please Enter Your Password");
+        labelWarnPassword.setBounds(25, 175, 300, 50);
+        labelWarnPassword.setFont(new Font(null, Font.ITALIC, 12));
+        labelWarnPassword.setForeground(Color.red);
 
         // **create btnCreateAccount */
         btnCreateAccount = new JButton("Create Account");
-        btnCreateAccount.setBounds(25, 265, 170, 35);
+        btnCreateAccount.setBounds(25, 220, 170, 35);
         btnCreateAccount.setFocusable(false);
         btnCreateAccount.setBackground(new Color(38, 78, 202));
         btnCreateAccount.setForeground(Color.white);
+        btnCreateAccount.addActionListener(this);
 
         // **create btnBack */
         btnBack = new JButton("Back");
-        btnBack.setBounds(200, 265, 100, 35);
+        btnBack.setBounds(200, 220, 100, 35);
         btnBack.setFocusable(false);
         btnBack.setBackground(new Color(38, 78, 202));
         btnBack.setForeground(Color.white);
@@ -104,8 +104,6 @@ public class RegisterUser extends JFrame implements ActionListener {
         // **add components to panelCenter */
         panelCenter.add(btnBack);
         panelCenter.add(btnCreateAccount);
-        panelCenter.add(passwordFieldConfirm);
-        panelCenter.add(labelPassConfirm);
         panelCenter.add(passwordField);
         panelCenter.add(labelPassword);
         panelCenter.add(textFieldUsername);
@@ -124,6 +122,49 @@ public class RegisterUser extends JFrame implements ActionListener {
         if (e.getSource() == btnBack) {
             this.dispose();
             new Home();
+        }
+
+        if (e.getSource() == btnCreateAccount) {
+
+            RegisterUser.this.setVisible(false);
+
+            // **registration form validation */
+            if (textFieldUsername.getText().trim().isEmpty() && passwordField.getText().trim().isEmpty()) {
+                panelCenter.add(labelWarnUsername);
+                panelCenter.add(labelWarnPassword);
+            } else {
+                panelCenter.remove(labelWarnUsername);
+                panelCenter.remove(labelWarnPassword);
+            }
+
+            if (textFieldUsername.getText().trim().isEmpty()) {
+                panelCenter.add(labelWarnUsername);
+            } else {
+                panelCenter.remove(labelWarnUsername);
+            }
+
+            if (passwordField.getText().trim().isEmpty()) {
+                panelCenter.add(labelWarnPassword);
+            } else {
+                panelCenter.remove(labelWarnPassword);
+            }
+
+            RegisterUser.this.setVisible(true);
+
+            if (textFieldUsername.getText().length() > 0 && passwordField.getText().length() > 0) {
+                // **INSERT statement */
+                String username = textFieldUsername.getText();
+                String password = new String(passwordField.getPassword()) ;
+                
+                if(LogicBuilding.insertData("INSERT INTO users (username, password) VALUES ('" + username + "', '" + password + "');")){
+                    JOptionPane.showMessageDialog(null, "Add User Succesfully", "Add User", JOptionPane.INFORMATION_MESSAGE);
+                    this.dispose();
+                    new Home();
+                }else{
+                    JOptionPane.showMessageDialog(null, "Add User Unsuccessful", "Add User", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+
         }
     }
 
