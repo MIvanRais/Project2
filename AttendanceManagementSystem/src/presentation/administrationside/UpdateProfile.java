4389df9f06ae645f117a4ab69f11ce706630ae5d
MIvanRais <26ivanrais@gmail.com>
2022-05-12 10:15:14 +0700

    create GUI UpdateProfile page

--- AttendanceManagementSystem/src/presentation/administrationside/UpdateProfile.java
+++ AttendanceManagementSystem/src/presentation/administrationside/UpdateProfile.java
@@ -1,16 +1,12 @@
 package presentation.administrationside;
 
-import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -18,164 +14,172 @@ import java.awt.event.ActionListener;
 
 public class UpdateProfile extends JFrame implements ActionListener {
 
-    JPanel panelNorth, panelCenter;
-    JLabel labelHeader, labelHeaderPanelCenter, labelFirstName, labelLastName, labelFullName, labelEmail;
-    JTextField textFieldFirstName, textFieldLastName, textFieldFullName, textFieldEmail;
-    JMenuBar menuBar;
-    JMenu adminMenu;
-    JMenuItem changePasswordMenu, logoutMenu, homeMenu;
+    JPanel panelWest, panelEast, panelSouth, panelLineWest;
+    JLabel labelProfile, labelFirstName, labelLastName, labelEmail, labelProject, labelGender, labelNoTelephone, labelFooter;
+    JTextField textFieldFirstName, textFieldLastName, textFieldNoTlp, textFieldEmail;
     JButton btnSave;
+    JRadioButton btnMale, btnFemale;
+    ButtonGroup btnGroup;
 
     UpdateProfile() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Profile");
-        this.setSize(1200, 800);
+        this.setTitle("Update Profile");
+        this.setSize(600, 650);
         this.setResizable(false);
         this.setLayout(null);
 
-        menuBar = new JMenuBar(); // **container for menu */
-        menuBar.add(Box.createHorizontalGlue()); // **set menuBar position to right-align */
+        // **create panelWest
+        panelWest = new JPanel();
+        panelWest.setBounds(0, 0, 250, 550);
+        panelWest.setBackground(new Color(33, 72, 192));
+        panelWest.setLayout(null);
 
-        // **create adminMenu */
-        adminMenu = new JMenu("Mochammad Ivan Ra'is");
-        Font setFont = new Font("sans-serif", Font.BOLD, 14);
-        UIManager.put("Menu.font", setFont);
+        // **create labelProfile
+        labelProfile = new JLabel("Update Profile");
+        labelProfile.setBounds(0, 250, 250, 50);
+        labelProfile.setFont(new Font(null, Font.BOLD, 25));
+        labelProfile.setForeground(Color.white);
+        labelProfile.setHorizontalAlignment(JLabel.CENTER);
 
-        // **create menuItem */
-        homeMenu = new JMenuItem("Home");
-        homeMenu.addActionListener(this);
-        changePasswordMenu = new JMenuItem("Change Password");
-        changePasswordMenu.addActionListener(this);
-        logoutMenu = new JMenuItem("Logout");
-        logoutMenu.addActionListener(this);
+        // **create panelLineWest
+        panelLineWest = new JPanel();
+        panelLineWest.setBounds(100, 290, 80, 2);
+        panelLineWest.setBackground(Color.white);
 
-        // **add component (menu item) to menu */
-        adminMenu.add(homeMenu);
-        adminMenu.add(changePasswordMenu);
-        adminMenu.add(logoutMenu);
+        // **add component to panelWest
+        panelWest.add(labelProfile);
+        panelWest.add(panelLineWest);
 
-        // **add component (menu) to menu bar */
-        menuBar.add(adminMenu);
+        // **create panelEast
+        panelEast = new JPanel();
+        panelEast.setBounds(250, 0, 350, 550);
+        panelEast.setLayout(null);
+        panelEast.setBackground(Color.white);
+        // panelEast.setBackground(Color.cyan);
 
-        // **create panelNorth */
-        panelNorth = new JPanel();
-        panelNorth.setBounds(0, 0, 1200, 100);
-        panelNorth.setBackground(new Color(33, 72, 192));
-
-        // **create labelHeader */
-        labelHeader = new JLabel("Detail Profile");
-        labelHeader.setBounds(0, 0, 1200, 100);
-        labelHeader.setFont(new Font(null, Font.BOLD, 25));
-        labelHeader.setForeground(Color.white);
-        labelHeader.setHorizontalAlignment(JLabel.CENTER);
-
-        // **create panelCenter */
-        panelCenter = new JPanel();
-        panelCenter.setBounds(350, 150, 450, 500);
-        panelCenter.setBackground(Color.white);
-        panelCenter.setLayout(null);
-
-        // **create labelHeaderPanelCenter */
-        labelHeaderPanelCenter = new JLabel("Edit Profile");
-        labelHeaderPanelCenter.setForeground(new Color(33, 72, 192));
-        labelHeaderPanelCenter.setFont(new Font(null, Font.PLAIN, 18));
-        labelHeaderPanelCenter.setBounds(25, 30, 100, 35);
-
-        // **create labelFirstName */
-        labelFirstName = new JLabel("First Name");
+        // **create labelFirstName
+        labelFirstName = new JLabel("First Name     :");
+        labelFirstName.setBounds(20, 50, 115, 30);
         labelFirstName.setFont(new Font(null, Font.PLAIN, 15));
-        labelFirstName.setForeground(new Color(148, 164, 175));
-        labelFirstName.setBounds(25, 90, 90, 45);
+        labelFirstName.setForeground(Color.black);
+        // labelFirstName.setBorder(BorderFactory.createLineBorder(Color.black));
 
         // **create textFieldFirstName
         textFieldFirstName = new JTextField();
-        textFieldFirstName.setBounds(25, 130, 180, 45);
-        textFieldFirstName.setSize(180, 35);
-        textFieldFirstName.setFont(new Font(null, Font.BOLD, 14));
+        textFieldFirstName.setBounds(20, 95, 300, 30);
+        textFieldFirstName.setFont(new Font(null,Font.PLAIN,15));
 
-        // **create labelLastName */
-        labelLastName = new JLabel("Last Name");
+        // **create labelLastName
+        labelLastName = new JLabel("Last Name     :");
+        labelLastName.setBounds(20, 140, 115, 30);
         labelLastName.setFont(new Font(null, Font.PLAIN, 15));
-        labelLastName.setForeground(new Color(148, 164, 175));
-        labelLastName.setBounds(250, 90, 90, 45);
+        labelLastName.setForeground(Color.black);
+        // labelLastName.setBorder(BorderFactory.createLineBorder(Color.black));
 
         // **create textFieldLastName
         textFieldLastName = new JTextField();
-        textFieldLastName.setBounds(250, 130, 180, 45);
-        textFieldLastName.setSize(180, 35);
-        textFieldLastName.setFont(new Font(null, Font.BOLD, 14));
+        textFieldLastName.setBounds(20, 180, 300, 30);
+        textFieldLastName.setFont(new Font(null,Font.PLAIN,15));
 
-        // **create labelFullName */
-        labelFullName = new JLabel("Full Name");
-        labelFullName.setFont(new Font(null, Font.PLAIN, 15));
-        labelFullName.setForeground(new Color(148, 164, 175));
-        labelFullName.setBounds(25, 180, 90, 45);
+        // **create labelGender
+        labelGender = new JLabel("Gender          :");
+        labelGender.setBounds(20, 225, 115, 30);
+        labelGender.setFont(new Font(null, Font.PLAIN, 15));
+        labelGender.setForeground(Color.black);
+        // labelGender.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        // **create textFieldFullName
-        textFieldFullName = new JTextField();
-        textFieldFullName.setBounds(25, 220, 180, 45);
-        textFieldFullName.setSize(405, 35);
-        textFieldFullName.setFont(new Font(null, Font.BOLD, 14));
+        // **create btnMale
+        btnMale = new JRadioButton("Male");
+        btnMale.setBounds(20, 270, 95, 30);
+        btnMale.setFont(new Font(null, Font.PLAIN, 15));
+        btnMale.setBackground(Color.white);
+        btnFemale = new JRadioButton("Female");
+        btnFemale.setBounds(125, 270, 95, 30);
+        btnFemale.setFont(new Font(null, Font.PLAIN, 15));
+        btnFemale.setBackground(Color.white);
+
+        // **create btnGroup
+        btnGroup = new ButtonGroup();
+
+        // **add components to btnGroup
+        btnGroup.add(btnMale);
+        btnGroup.add(btnFemale);
+
+        // **create labelNoTelephone
+        labelNoTelephone = new JLabel("No. Telephone:");
+        labelNoTelephone.setBounds(20, 310, 115, 30);
+        labelNoTelephone.setFont(new Font(null, Font.PLAIN, 15));
+        labelNoTelephone.setForeground(Color.black);
+        // labelNoTelephone.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        // **create textFieldNoTlp
+        textFieldNoTlp = new JTextField();
+        textFieldNoTlp.setBounds(20, 355, 300, 30);
+        textFieldNoTlp.setFont(new Font(null,Font.PLAIN,15));
 
         // **create labelEmail
-        labelEmail = new JLabel("Email");
+        labelEmail = new JLabel("Email             :");
+        labelEmail.setBounds(20, 400, 115, 30);
         labelEmail.setFont(new Font(null, Font.PLAIN, 15));
-        labelEmail.setForeground(new Color(148, 164, 175));
-        labelEmail.setBounds(25, 270, 90, 45);
+        labelEmail.setForeground(Color.black);
+        // labelEmail.setBorder(BorderFactory.createLineBorder(Color.black));
 
         // **create textFieldEmail
         textFieldEmail = new JTextField();
-        textFieldEmail.setBounds(25, 310, 180, 45);
-        textFieldEmail.setSize(405, 35);
-        textFieldEmail.setFont(new Font(null, Font.BOLD, 14));
+        textFieldEmail.setBounds(20, 445, 300, 30);
+        textFieldEmail.setFont(new Font(null,Font.PLAIN,15));
 
-        // **create btnSave */
+        // **create btnUpdate
         btnSave = new JButton("Save");
-        btnSave.setBounds(25, 360, 100, 35);
+        btnSave.setBounds(217, 500, 100, 35);
         btnSave.setFocusable(false);
         btnSave.setBackground(new Color(38, 78, 202));
         btnSave.setForeground(Color.white);
 
-        // **add components to panelCenter */
-        panelCenter.add(btnSave);
-        panelCenter.add(textFieldEmail);
-        panelCenter.add(labelEmail);
-        panelCenter.add(textFieldFullName);
-        panelCenter.add(labelFullName);
-        panelCenter.add(textFieldLastName);
-        panelCenter.add(labelLastName);
-        panelCenter.add(textFieldFirstName);
-        panelCenter.add(labelFirstName);
-        panelCenter.add(labelHeaderPanelCenter);
-        panelCenter.add(labelHeaderPanelCenter);
+        // **add components to panelEast
+        panelEast.add(labelFirstName);
+        panelEast.add(labelLastName);
+        panelEast.add(labelGender);
+        panelEast.add(labelNoTelephone);
+        panelEast.add(labelEmail);
+        panelEast.add(btnSave);
+        panelEast.add(textFieldFirstName);
+        panelEast.add(textFieldLastName);
+        panelEast.add(textFieldNoTlp);
+        panelEast.add(textFieldEmail);
+        panelEast.add(btnMale);
+        panelEast.add(btnFemale);
 
-        // **add components to frame */
-        this.add(panelCenter);
-        this.setJMenuBar(menuBar);
-        this.add(labelHeader);
-        this.add(panelNorth);
+        // **create panelSouth
+        panelSouth = new JPanel();
+        panelSouth.setBounds(0, 550, 600, 100);
+        panelSouth.setBackground(new Color(33, 72, 192));
+        panelSouth.setLayout(null);
+        // panelSouth.setBackground(new Color(33, 72, 192));
+
+        // **create labelFooter */
+        labelFooter = new JLabel("Admin Attendance Management System");
+        labelFooter.setBounds(0, 0, 600, 50);
+        labelFooter.setFont(new Font(null, Font.BOLD, 20));
+        labelFooter.setForeground(Color.white);
+        labelFooter.setHorizontalAlignment(JLabel.CENTER);
+        // labelFooter.setVerticalTextPosition((int) JLabel.CENTER_ALIGNMENT);
+        // labelFooter.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        // **add component to panelSouth
+        panelSouth.add(labelFooter);
+
+        // **add components to frame
+        this.add(panelWest);
+        this.add(panelEast);
+        this.add(panelSouth);
         this.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == homeMenu) {
-            // **when click homeMenu item, it is going to open home page */
-            this.dispose();
-            new Home();
-        } else if (e.getSource() == changePasswordMenu) {
-            // **when click changePasswordMenu item, it is going to open change password
-            // page */
-            this.dispose();
-            new ChangePassword();
-        } else {
-            int answer;
-            answer = JOptionPane.showConfirmDialog(null, "Are you sure want to logout?", "Logout",
-                    JOptionPane.YES_NO_OPTION);
-            // **if you click yes option, it is going to close the frame
-            if (answer == 0) {
-                this.dispose();
-            }
-        }
+        // TODO Auto-generated method stub
+
     }
+
 }
