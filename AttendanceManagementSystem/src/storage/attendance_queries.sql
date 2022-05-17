eb7c6894096f38898128e0cf38cc6414941811ce
MIvanRais <26ivanrais@gmail.com>
2022-05-17 16:43:27 +0700

    create trigger tr_admins_ai and create table admin_details

--- AttendanceManagementSystem/src/storage/attendance_queries.sql
+++ AttendanceManagementSystem/src/storage/attendance_queries.sql
@@ -13,6 +13,22 @@ CREATE TABLE admins(
     registration_date DATE DEFAULT (CURRENT_DATE())
 );
 
+--**create tr_admins_ai
+CREATE TRIGGER tr_admins_ai AFTER INSERT ON admins FOR EACH ROW
+INSERT INTO admin_details (admin_username) values (new.username);
+
+--**create admin_details
+CREATE TABLE admin_details(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    gender VARCHAR(6),
+    no_telephone VARCHAR(12),
+    email VARCHAR(30),
+    admin_username VARCHAR(30),
+    FOREIGN KEY(admin_username) REFERENCES admins(username)
+);
+
 --**create table users
 CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -21,33 +37,18 @@ CREATE TABLE users(
     registration_date DATE DEFAULT (CURRENT_DATE())
 );
 
---**database cloning
-CREATE DATABASE clone_db;
+--**create tr_users_ai
+CREATE TRIGGER tr_users_ai AFTER INSERT ON users FOR EACH ROW
+INSERT INTO user_details (user_username) values (new.username);
 
-USE clone_db;
-
-CREATE TABLE admins(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) UNIQUE,
-    password VARCHAR(15),
-    registration_date DATE DEFAULT (CURRENT_DATE())
-);
-
-CREATE TABLE admin_details(
+--**create user_details
+CREATE TABLE user_details(
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     gender VARCHAR(6),
-    no_telephone CHAR(12),
+    no_telephone VARCHAR(12),
     email VARCHAR(30),
-    admin_username VARCHAR(30),
-    FOREIGN KEY(admin_username) REFERENCES admins(username)
+    user_username VARCHAR(30),
+    FOREIGN KEY(user_username) REFERENCES users(username)
 );
-
-CREATE TRIGGER tr_admins_ai AFTER INSERT ON admins FOR EACH ROW
-INSERT INTO admin_details (admin_username) values (new.username);
-
-UPDATE admin_details SET first_name = 'Udin', last_name = 'uhuy', gender = 'male', no_telephone = '21212324', email = 'udin@gmail.com' WHERE admin_username = 'admin';
-
-select username, first_name, last_name, gender, no_telephone, email from admins 
-inner join admin_details on admins.username = admin_details.admin_username;
